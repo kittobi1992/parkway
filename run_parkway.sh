370f54a35fc8c5235e8b238514f7770af558b7a9
Tobias Heuer <tobias.heuer@gmx.net>
2020-03-30 17:48:23 +0200

    add config

--- run_parkway.sh
+++ run_parkway.sh
@@ -7,4 +7,4 @@ EPSILON=$4
 BIN_HYPERGRAPH=$1.bin
 
 $PARKWAY_CONVERTER -h $HYPERGRAPH -p $NUM_PROCESSORS
-mpirun -N $NUM_PROCESSORS $PARKWAY -p $NUM_BLOCKS -c $EPSILON --display-info --display-progress --hypergraph=$BIN_HYPERGRAPH
+mpirun -N $NUM_PROCESSORS $PARKWAY -p $NUM_BLOCKS -c $EPSILON --recursive-bisection.number-of-runs=$NUM_PROCESSORS -o config.ini --hypergraph=$BIN_HYPERGRAPH
